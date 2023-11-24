149c63ae7676a8ab2fdcb0f69b586e5c72602498
javlon1 <muhamedmijotot@gmail.com>
2023-11-24 10:33:11 +0300

    faq

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,9 +12,35 @@ window.addEventListener('scroll', () => {
 })
 
 
-scrollUp.addEventListener("click", ()=>{
+scrollUp.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     })
 })
+
+
+
+// Добавляем обработчик событий для отслеживания изменения состояния аккордеона 
+document.addEventListener('shown.bs.collapse', function (event) {
+    var activeHeader = event.target.parentElement.querySelector('.button');
+    if (activeHeader) {
+        activeHeader.classList.add('myActive');
+    }
+});
+
+document.addEventListener('hidden.bs.collapse', function (event) {
+    var activeHeader = event.target.parentElement.querySelector('.button');
+    if (activeHeader) {
+        activeHeader.classList.remove('myActive');
+    }
+}); 
+
+
+document.addEventListener("DOMContentLoaded", function () { 
+    // Развернуть первый элемент аккордеона 
+    var firstAccordionItem = document.querySelector('.accordion-item:first-child'); 
+    var firstCollapse = firstAccordionItem.querySelector('.accordion-collapse'); 
+     
+    firstCollapse.classList.add('myActive'); 
+});
