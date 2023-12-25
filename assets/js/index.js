41b18005aee1bd3b0893e51fa6991dbf285b8e8f
Javlon1 <muhamedmijotot@gmail.com>
2023-12-25 14:44:43 +0500

    inspect

--- assets/js/index.js
+++ assets/js/index.js
@@ -119,3 +119,32 @@ const time = 30000
 
 setTimeout(popUp, time);
 
+//
+document.addEventListener('DOMContentLoaded', function () {
+    const handleContextMenu = function (e) {
+        e.preventDefault();
+    };
+
+    const handleKeyDown = function (e) {
+        if (e.keyCode === 123) {
+            e.preventDefault();
+        } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+            e.preventDefault();
+        } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+            e.preventDefault();
+        } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+            e.preventDefault();
+        } else if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+            e.preventDefault();
+        }
+    };
+
+    document.addEventListener('contextmenu', handleContextMenu);
+    document.addEventListener('keydown', handleKeyDown);
+
+    window.addEventListener('beforeunload', function () {
+        document.removeEventListener('contextmenu', handleContextMenu);
+        document.removeEventListener('keydown', handleKeyDown);
+    });
+});
+//
